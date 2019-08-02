88910ac5e51c58726f45584a12561bf4784b72c9
Dorell James Galang <galangdj@gmail.com>
2019-08-03 01:43:20 +0800

    Todo CRUD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const TodoModel = {
+  all() {
+    return axios.get("http://localhost:1337/todos");
+  },
+
+  read(id) {},
+
+  create(data) {
+    const { title } = data;
+    return axios.post("http://localhost:1337/todos", {
+      title
+    });
+  },
+
+  update(data) {
+    const { id, title } = data;
+    return axios.put("http://localhost:1337/todos/" + id, {
+      title
+    });
+  },
+
+  delete(id) {
+    return axios.delete("http://localhost:1337/todos/" + id);
+  }
+};
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    TodoModel.all().then(response => setTodos(response.data));
+  }, []);
+
+  function handleSubmitTodo(e) {
+    const newTodo = e.target.querySelector("input[type='text']").value;
+    TodoModel.create({ title: newTodo }).then(addedTodo =>
+      setTodos([...todos, addedTodo.data])
+    );
+
+    e.target.querySelector("input[type='text']").value = "";
+
+    e.preventDefault();
+  }
+
+  function updateTodo(e, id) {
+    console.log(e.target.innerText, id);
+    const title = e.target.innerText;
+    if (e.key === "Enter") {
+      TodoModel.update({ title, id });
+
+      e.preventDefault();
+    }
+  }
+
+  function deleteTodo(id) {
+    console.log(id);
+    TodoModel.delete(id).then(deletedTodo =>
+      setTodos(todos.filter(todo => todo.id !== deletedTodo.data.id))
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form method="post" onSubmit={handleSubmitTodo}>
+        <input type="text" placeholder="What needs to be done?  " />
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <span
+              contentEditable="true"
+              onKeyPress={e => updateTodo(e, todo.id)}
+            >
+              {todo.title}
+            </span>{" "}
+            <button onClick={e => deleteTodo(todo.id)}>x</button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
